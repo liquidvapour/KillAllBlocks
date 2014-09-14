6d2463bfe001a259d0fd16353287548e81122eb6
liquidvapour <liquidvapour@gmail.com>
2014-09-14 16:16:07 +0100

    sound teaking

--- soundbox.lua
+++ soundbox.lua
@@ -3,10 +3,10 @@ local Class = require "lib.middleclass"
 local SoundBox = Class("SoundBox")
 
 function SoundBox:initialize()
-    self.pip = love.audio.newSource("resources/sound/pip.wav", "static")    
-    self.splat = love.audio.newSource("resources/sound/splat.wav", "static")    
-    self.plink = love.audio.newSource("resources/sound/plink.wav", "static")    
-    self.explosion = love.audio.newSource("resources/sound/explosion.wav", "static")    
+    self.pip = love.audio.newSource("resources/sound/pip02.wav", "static")    
+    self.splat = love.audio.newSource("resources/sound/splat01.wav", "static")    
+    self.saw01 = love.audio.newSource("resources/sound/saw01.wav", "static")    
+    self.explosion = love.audio.newSource("resources/sound/explosion01.wav", "static")    
     self.wobly = love.audio.newSource("resources/sound/wobly.wav", "static")    
 end
 
@@ -27,7 +27,7 @@ function SoundBox:hitTarget()
 end
 
 function SoundBox:hitPaddle()
-    playSource(self.plink)
+    playSource(self.saw01)
 end
 
 function SoundBox:hitGoal()
