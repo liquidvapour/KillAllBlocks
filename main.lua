c917be0412784fc156ad395e24c49fd9378ed50e
liquidvapour <liquidvapour@gmail.com>
2014-09-15 12:47:18 +0100

    first take

--- main.lua
+++ main.lua
@@ -8,13 +8,19 @@ require "menu"
 require "gameover"
 require "captureName"
 
+local canvas
+local screenQuad
+
 function love.load()
-    local result = love.window.setMode(800, 600, {fullscreen = false})
+    local result = love.window.setMode(1024, 768, {fullscreen = false})
     print(string.format("setMode result: %s", result))
     print("width: "..love.window.getWidth()..", height:"..love.window.getHeight())
 
     myGame = game:new()
     myGame:gotoState("menu")
+    
+    canvas = love.graphics.newCanvas(800, 600)
+    screenQuad = love.graphics.newQuad(0, 0, 1024, 768, 800, 600)
 end
 
 function love.update(dt)
@@ -22,7 +28,12 @@ function love.update(dt)
 end
 
 function love.draw()
+    canvas:clear()
+    love.graphics.setCanvas(canvas)
     myGame:draw()
+    love.graphics.setCanvas()
+    
+    love.graphics.draw(canvas, screenQuad, 0, 0)
 end
 
 -- Non-player keypresses
