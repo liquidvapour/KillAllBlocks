b8236aa58bdbbf6b76715ee8e407a7466121ada1
liquidvapour <liquidvapour@gmail.com>
2014-12-27 17:54:25 +0000

    added use of config.lua

---
+++ conf.lua
@@ -0,0 +1,11 @@
+print("config triggered")
+
+function love.conf(t)
+    t.window.title = "Kill All Blocks"
+    t.window.width = 1024
+    t.window.height = 768
+    t.window.fullscreen = true
+    t.window.vsync = false
+    
+    t.disgnostics.enabled = true
+end
--- main.lua
+++ main.lua
@@ -70,19 +70,13 @@ local function getScreenMesh(canvas)
 end
 
 function love.load()
-    --local outputWidth, outputHeight = 800, 600
-    local outputWidth, outputHeight = 1024, 768
-    --local outputWidth, outputHeight = 1920, 1080
-    --local result = love.window.setMode(1024, 768, {fullscreen = false})
-    local fullscreen = false
-    local result = love.window.setMode(outputWidth, outputHeight, {fullscreen = fullscreen})
-    --local result = love.window.setMode(outputWidth, outputHeight, {fullscreen = true})
     print(string.format("setMode result: %s", result))
     print("width: "..love.window.getWidth()..", height:"..love.window.getHeight())
 
     local soundbox = SoundBox:new()
     
     myGame = game:new(soundbox)
+    
     myGame:gotoState("menu")
     
     canvas = love.graphics.newCanvas(sceneWidth, sceneHeight)
